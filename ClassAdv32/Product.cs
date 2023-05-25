91d7a4acf3bf0a1c410b165d7f3dcfa950c25e02
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-25 09:05:44 +0900

    소멸자 예제 (역순으로 소멸된다)

---
+++ ClassAdv32/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassAdv32
+{
+    internal class Product
+    {
+        private string name;
+        private int price;
+
+        //생성자
+        public Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        //소멸자
+        ~Product() //객체가 소멸될때
+        {
+            Console.WriteLine(this.name + "의 소멸자 호출");
+
+        }
+    }
+}
--- ClassAdv32/Program.cs
+++ ClassAdv32/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace ClassAdv32
     {
         static void Main(string[] args)
         {
+            //소멸자 예시
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("아이스크림", 1000);
+            Product product3 = new Product("케이크", 1000);
+
+            /*
+             * 객체 생성의 역순으로 소멸된다
+             케이크의 소멸자 호출
+             아이스크림의 소멸자 호출
+             과자의 소멸자 호출
+            */
+
+
         }
     }
 }
